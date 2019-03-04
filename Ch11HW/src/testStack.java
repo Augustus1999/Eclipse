6217dc6e6f0070d309f9cf74f5db8453ca9c372b
TilerRose <roset@student.swosu.edu>
2019-03-04 13:44:17 -0600

    added code to Ch11HW

--- Ch11HW/src/testStack.java
+++ Ch11HW/src/testStack.java
@@ -5,10 +5,15 @@ public class testStack {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		ArrayList<Object> list = new ArrayList<>();
+		//ArrayList<Object> list = new ArrayList<>();
 		Scanner InputFromUser = new Scanner(System.in);
-		int GetFromUser = InputFromUser.nextInt(); 
-		System.out.println(GetFromUser);
+		String GetFromUser1 = InputFromUser.next();
+		String GetFromUser2 = InputFromUser.next();
+		String GetFromUser3 = InputFromUser.next();
+		String GetFromUser4 = InputFromUser.next();
+		String GetFromUser5 = InputFromUser.next();
+		System.out.println(GetFromUser5 + " " + GetFromUser4 + " " + GetFromUser3 + " " + GetFromUser2 + " " + GetFromUser1 + " ");
+		
 	}
 
 }
