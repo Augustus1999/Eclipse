7eed4a41a0ab3417c97b2957ee87fef1765451ce
TilerRose <roset@student.swosu.edu>
2019-03-04 13:30:58 -0600

    added Ch11HW

---
+++ Ch11HW/src/testStack.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class testStack {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		ArrayList<Object> list = new ArrayList<>();
+		Scanner InputFromUser = new Scanner(System.in);
+		int GetFromUser = InputFromUser.nextInt(); 
+		System.out.println(GetFromUser);
+	}
+
+}
