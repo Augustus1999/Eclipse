ef51b0c1057ea16914301270c5e0895083637599
TilerRose <roset@student.swosu.edu>
2019-03-27 13:29:25 -0500

    added chapter 13 Homework

---
+++ Ch13/src/Cs2Ch13Hw/MainDecimalToFraction.java
@@ -0,0 +1,18 @@
+package Cs2Ch13Hw;
+
+import java.util.Scanner;
+
+public class MainDecimalToFraction {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double inputFromuser = scanner.nextDouble();
+		int start = 0;
+		System.out.println(inputFromuser);
+		
+		
+		
+
+	}
+
+}
---
+++ testStuff/src/testStuff/test.java
@@ -0,0 +1,11 @@
+package testStuff;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		int[] list = new int[5];
+		System.out.println(list[5]);
+		}
+		}
