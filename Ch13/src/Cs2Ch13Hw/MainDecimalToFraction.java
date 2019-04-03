6cac3bcb2a3bfa422fea9200e9737f84de48d7fc
TilerRose <roset@student.swosu.edu>
2019-04-03 13:28:46 -0500

    added code to Ch13

--- Ch13/src/Cs2Ch13Hw/MainDecimalToFraction.java
+++ Ch13/src/Cs2Ch13Hw/MainDecimalToFraction.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class MainDecimalToFraction {
 
 	public static void main(String[] args) {
+		System.out.println("Input a decimal");
 		Scanner scanner = new Scanner(System.in);
 		double inputFromuser = scanner.nextDouble();
-		int start = 0;
+		//int start = 0;
 		System.out.println(inputFromuser);
-		
-		
-		
-
+			
 	}
 
 }
